90186754241720754a25c2daca9efa2255094a4e
yujeen02 <yujin5479@gmail.com>
2025-03-06 19:18:10 +0900

    feat. 0306 커밋

--- static/js/write.js
+++ static/js/write.js
@@ -1,19 +1,18 @@
 // 에디터
 const ed = document.querySelector("#editor");
-const comment = ed.dataset.inputComment;
 
 const editor = new toastui.Editor({
-  el: document.querySelector("#content"), // 에디터를 적용할 요소 (컨테이너)
-  height: "300px", // 에디터 영역의 높이 값 (OOOpx || auto)
-  initialEditType: "markdown", // 최초로 보여줄 에디터 타입 (markdown || wysiwyg)
-  initialValue: comment, // 내용의 초기 값으로, 반드시 마크다운 문자열 형태여야 함
-  previewStyle: "vertical", // 마크다운 프리뷰 스타일 (tab || vertical)
+  el: document.querySelector("#content"),
+  height: "300px",
+  initialEditType: "markdown",
+  initialValue: "글 작성",
+  previewStyle: "vertical",
 });
 
 // 글 저장
 const write = async () => {
   const title = document.querySelector("input[name='title']").value;
-  const gender = document.querySelector("input[name='gender']:checked").value;
+  const content = editor.getMarkdown();
 
   try {
     const response = await axios.post("/post/create", {});
