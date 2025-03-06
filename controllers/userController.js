00dc5ccf9c70b6aedb50eda41fe43160d967554b
yujeen02 <yujin5479@gmail.com>
2025-03-06 16:01:15 +0900

    feat. 유저 db 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,8 @@ const checkEmail = async (req, res) => {
     if (!email) {
       return res.status(400).json({ message: "이메일을 입력해 주세요." });
     }
-    const existingUser = await User.findOne({ where: { email } });
+
+    const existingUser = await User.findOne({ where: { email: email } });
 
     if (existingUser) {
       return res.status(400).json({ message: "이미 사용 중인 이메일입니다." });
@@ -38,25 +39,14 @@ const registerUser = async (req, res) => {
       phone,
     } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const formatBirth = new Date(birthDate).toISOString().split("T")[0];
     const formatPhone = phone
       .replace(/[^0-9]/g, "")
       .replace(/^(\d{3})(\d{3,4})(\d{4})$/, "$1-$2-$3");
 
-    const pw =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    if (!pw.test(password)) {
-      return res.status(400).json({
-        message:
-          "비밀번호는 최소 8자 이상, 대소문자, 숫자, 특수문자를 포함해야 합니다.",
-      });
-    }
-
     const hashedPassword = await bcrypt.hash(password, 10);
 
-
     const formatBirth = new Date(birthDate).toISOString().split("T")[0];
+
     const user = await User.create({
       email,
       username,
@@ -80,16 +70,16 @@ const registerUser = async (req, res) => {
 // 로그인
 const loginUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, pass } = req.body;
 
     // 사용자 확인
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email: email } });
     if (!user) {
       return res.status(404).json({ message: "사용자를 찾을 수 없음" });
     }
 
     // 비밀번호 검증
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(pass, user.password);
     if (!isMatch) {
       return res.status(401).json({ message: "비밀번호가 일치하지 않음" });
     }
