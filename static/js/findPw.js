6fcfb5edb94e11a07a574c840f8a96ad21d8fdf9
yujeen02 <yujin5479@gmail.com>
2025-03-07 15:07:56 +0900

    feat. 비밀번호

--- static/js/findPw.js
+++ static/js/findPw.js
@@ -1,3 +1,32 @@
+// 비밀번호 찾기
+const idCheck = async () => {
+  const email = document.getElementById("email").value;
+
+  const data = { email };
+
+  try {
+    const res = await axios.post("/user/findPw", data);
+
+    if (res.data.email) {
+      Swal.fire({
+        title: `새 비밀번호를 저장하시겠습니까`,
+        icon: "success",
+      });
+    } else {
+      Swal.fire({
+        title: "해당 정보로 가입된 아이디가 없습니다.",
+        icon: "error",
+      });
+    }
+  } catch (error) {
+    Swal.fire({
+      title: "서버 오류 발생",
+      icon: "error",
+    });
+    console.error("Error:", error);
+  }
+};
+
 // 페이지 이동
 function moveUrl(url) {
   window.location.href = `/postit/${url}`;
