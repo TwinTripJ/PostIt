dbd7bfa022205c95670c847b98babe234d6e04f6
yujeen02 <yujin5479@gmail.com>
2025-03-14 14:59:06 +0900

    feat. 팝업

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -4,5 +4,7 @@ const mainController = require("../controllers/mainController");
 
 router.get("/search", mainController.searchTitle);
 router.get("/:url", mainController.moveUrl);
+router.get("/popup", mainController.popUp);
+router.get("/popupClose", mainController.closePopup);
 
 module.exports = router;
